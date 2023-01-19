db686efe7e541dbbb6455a01222c52c77925831b
triinliis <triinu_liis@hotmail.com>
2023-01-19 23:28:29 +0200

    added Footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {" "}
+        <a href="https://beamish-pie-414eb4.netlify.app/" target="_blank">
+          Triin Liis
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/triinliis/tlk-react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
 
